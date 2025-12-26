f800f39ce0222dd74cc71d98598387d9850d898a
ishmamzarif <zarif.ishmam9@gmail.com>
2025-12-26 15:57:32 +0600

    added server and one test api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
+require("dotenv").config()
 const express = require("express");
 
 const app = express();
 
-const PORT = 3005
+app.get("/getUser", (req, res) => {
+    res.status(676).json({
+        status: "scuess",
+        user_name: "ishmam",
+    })
+})
 
-app.listen(PORT, () => {
-    console.log(`server is up and listening on port ${PORT}`);
+// if PORT env var defined use that, else use 33333
+const port = process.env.PORT || 3005
+
+console.log("test");
+
+app.listen(port, () => {
+    console.log(`server is up and listening on port ${port}`);
 });
