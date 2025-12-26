437ee1654c595a121c97d4a4308053ebe84cdbfb
ishmamzarif <zarif.ishmam9@gmail.com>
2025-12-26 16:16:59 +0600

    1:45

--- server/server.js
+++ server/server.js
@@ -3,12 +3,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/getUser", (req, res) => {
+// get all users
+app.get("/api/v1/users", (req, res) => {
     res.status(676).json({
-        status: "scuess",
-        user_name: "ishmam",
+        status: "success",
+        data: {
+            users: ["ishmam", "thuja"]
+        },
     })
 })
+
+// get one user
+app.get("/api/v1/users/:id", (req, res) => {
+    console.log(req.params);
+});
+
+// create a user
+app.post("/api/v1/users", (req, res) => {
+    console.log(req)
+});
+
+// delete a user
 
 // if PORT env var defined use that, else use 33333
 const port = process.env.PORT || 3005
